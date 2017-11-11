d1cc847245fd979e5e70f0c07630637248c98e2d
jaunerc <c.jauner@gmail.com>
2017-11-11 15:31:02 +0100

    Comments added.

--- minimax_rest_backend.pl
+++ minimax_rest_backend.pl
@@ -4,22 +4,27 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 
-:- http_handler(root(api), handle_api, []).
+% The api will be accessed through root/minimax.
+:- http_handler(root(minimax), handle_api, []).
 
+% rest_server(+Port)
+% Starts a http server instance.
 rest_server(Port) :-
         http_server(http_dispatch, [port(Port)]).
 
+% handle_api(+Request)
+% Handles an http request.
 handle_api(Request) :-
-   %format(user_output,"I'm here~n",[]),
    http_read_json(Request, DictIn,[json_object(term)]),
-   %format(user_output,"Request is: ~p~n",[Request]),
-   %format(user_output,"DictIn is: ~p~n",[DictIn]),
    solve(DictIn, DictOut),
    reply_json(DictOut).
 
+% JSON object for a tictactoe board.
 :- json_object
 	tictactoe(board:list).
-		
+
+% solve(+Query, -Solution)
+% Matches a Solution to the given query.
 solve(Query, Solution) :-
 	json_to_prolog(Query, tictactoe(Board)),
 	minimax(Board, BestMove),
